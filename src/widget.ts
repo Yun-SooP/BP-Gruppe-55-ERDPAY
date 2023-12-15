7a4acd269a17f3431eb37a2ce454e7b30068a8e5
Yeweon Park <yeweon.park1306@gmail.com>
2023-12-15 16:52:39 +0100

    linked transfer.html and balance.html each button respectively and changed variable names

--- src/widget.ts
+++ src/widget.ts
@@ -1,8 +1,10 @@
 const main = document.body.innerHTML = `
     <div>
         <button id="mainButton" type="button">Erdpay</button>
-        <div class="basePop" id="basePop">
-            <div class="mainPop" id="mainPop">
+        <div class="widget-body" id="widget-body">
+            <div>
+            </div>
+            <div class="button-container" id="button-container">
                 <button id="transfer">Transfer</button>
                 <button id="balance">View Account Balance</button>
                 <button id="close">Close</button>
@@ -12,17 +14,26 @@ const main = document.body.innerHTML = `
 `;
 
 const btn = document.querySelector('#mainButton');
-    btn?.addEventListener('click', () => {
-    const list = document.querySelector('.basePop')?.classList;
+btn?.addEventListener('click', () => {
+    const list = document.querySelector('.widget-body')?.classList;
     list?.add('open');
 });
 
 const btn2 = document.querySelector('#close');
 btn2?.addEventListener('click', () => {
-    const list = document.querySelector('.basePop')?.classList;
+    const list = document.querySelector('.widget-body')?.classList;
     list?.remove('open');
 });
 
+const btn3 = document.querySelector('#transfer');
+btn3?.addEventListener('click', () => {
+    window.location.href = '/src/transfer.html';
+});
+
+const btn4 = document.querySelector('#balance');
+btn4?.addEventListener('click', () => {
+    window.location.href = '/src/balance.html';
+});
 
 
 
