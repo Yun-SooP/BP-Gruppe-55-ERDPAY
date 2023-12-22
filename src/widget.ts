ff49ea34dfac42f4804b95c345db9c3b16c453c2
Marko0519 <xowjd0519@gmail.com>
2023-12-22 18:09:56 +0100

    fixed

--- src/widget.ts
+++ src/widget.ts
@@ -4,24 +4,29 @@ import { htmlCreateSessionForTransfer } from "./transfer";
 export function widget(html: HTMLDivElement) {
     html.innerHTML = `
         <div class="widget_body" id="widgetBodyMain">
-            <header class="widget__header">
-                <h3> Welcome to ErdPay</h3>
-            </header>
-            <form>
-                <button id="transfer">Transfer</button>
-                <button id="balance">View Account Balance</button>
-            </form>
         </div>
     `;
-
+    const body = document.querySelector<HTMLDivElement>('#widgetBodyMain')!
+    widgetBody(body)
+}
+export function widgetBody(html: HTMLDivElement){
+    html.innerHTML = `
+        <header class="widget__header">
+            <h3> Welcome to ErdPay</h3>
+        </header>
+        <form>
+            <button id="transfer">Transfer</button>
+            <button id="balance">View Account Balance</button>
+        </form>
+    `;
     const btn3 = document.querySelector<HTMLButtonElement>('#transfer');
     btn3?.addEventListener('click', () => {
-        htmlCreateSessionForTransfer(document.querySelector<HTMLDivElement>('#widgetBodyMain')!)
+        htmlCreateSessionForTransfer(html)
     });
 
     const btn4 = document.querySelector<HTMLButtonElement>('#balance')
     btn4?.addEventListener('click', () => {
-        htmlBalance(document.querySelector<HTMLDivElement>('#widgetBodyMain')!)
+        htmlBalance(html)
     })
 }
 
@@ -29,11 +34,15 @@ export function makeWidgetButton() {
     const html = document.querySelector<HTMLDivElement>('#app')!
     html.innerHTML = `
     <div>
-        <button id="mainButton" type="button"><img src="https://nifty.erdstall.dev/static/media/erdstall-logo.4ca5436f.png" class="logo" alt="TypeScript logo" /></button>
+        <button id="mainButton" type="button">
+            <img src="https://nifty.erdstall.dev/static/media/erdstall-logo.4ca5436f.png" class="logo" alt="TypeScript logo" />
+        </button>
     </div>
     `;
     const btn = document.querySelector('#mainButton');
-    btn?.addEventListener('click', () => widget(html))
+    btn?.addEventListener('click', () => {
+        widget(html)
+    })
 }
 
 makeWidgetButton()
