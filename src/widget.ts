b5facdfb7114a8b903f263feda5127eceb2328cf
Marko0519 <xowjd0519@gmail.com>
2023-12-22 17:30:25 +0100

    issue

--- src/widget.ts
+++ src/widget.ts
@@ -3,18 +3,25 @@ import { htmlCreateSessionForTransfer } from "./transfer";
 
 export function widget(html: HTMLDivElement) {
     html.innerHTML = `
-        <button id="transfer">Transfer</button>
-        <button id="balance">View Account Balance</button>
+        <div class="widget_body" id="widgetBodyMain">
+            <header class="widget__header">
+                <h3> Welcome to ErdPay</h3>
+            </header>
+            <form>
+                <button id="transfer">Transfer</button>
+                <button id="balance">View Account Balance</button>
+            </form>
+        </div>
     `;
 
-    const btn3 = document.querySelector('#transfer');
+    const btn3 = document.querySelector<HTMLButtonElement>('#transfer');
     btn3?.addEventListener('click', () => {
-        htmlCreateSessionForTransfer(document.querySelector<HTMLDivElement>('#app')!)
+        htmlCreateSessionForTransfer(document.querySelector<HTMLDivElement>('#widgetBodyMain')!)
     });
 
-    const btn4 = document.querySelector('#balance')
+    const btn4 = document.querySelector<HTMLButtonElement>('#balance')
     btn4?.addEventListener('click', () => {
-        htmlBalance(document.querySelector<HTMLDivElement>('#app')!)
+        htmlBalance(document.querySelector<HTMLDivElement>('#widgetBodyMain')!)
     })
 }
 
@@ -22,10 +29,10 @@ export function makeWidgetButton() {
     const html = document.querySelector<HTMLDivElement>('#app')!
     html.innerHTML = `
     <div>
-        <button id="widgetButton" type="button">Erdpay</button>
+        <button id="mainButton" type="button"><img src="https://nifty.erdstall.dev/static/media/erdstall-logo.4ca5436f.png" class="logo" alt="TypeScript logo" /></button>
     </div>
     `;
-    const btn = document.querySelector('#widgetButton');
+    const btn = document.querySelector('#mainButton');
     btn?.addEventListener('click', () => widget(html))
 }
 
