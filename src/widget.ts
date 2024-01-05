1e273e99a2518ffec396602b24d04f72f5f9fc54
Jvckk <cjaekwang@gmail.com>
2024-01-05 04:13:49 +0100

    merge widgetBody function to widget function

--- src/widget.ts
+++ src/widget.ts
@@ -1,74 +1,48 @@
 import { htmlBalance } from "./balance";
 import { htmlCreateSessionForTransfer } from "./transfer";
 
-
 export function widget(html: HTMLDivElement) {
-    html.innerHTML = `
+  html.innerHTML = `
         <div class="widget_body" id="widgetBodyMain">
-        </div>
-    `;
-    const body = document.querySelector<HTMLDivElement>('#widgetBodyMain')!
-    widgetBody(body)
-}
-export function widgetBody(html: HTMLDivElement){
-    html.innerHTML = `
-        <header class="widget__header">
-            <button class = "button logoButton" id="logoButton" type="button">
-                <img src="https://nifty.erdstall.dev/static/media/erdstall-logo.4ca5436f.png" class="logo" alt="TypeScript logo" />
-            </button>
+            <header class="widget__header">
+                <button class = "button logoButton" id="logoButton" type="button">
+                    <img src="https://nifty.erdstall.dev/static/media/erdstall-logo.4ca5436f.png" class="logo" alt="TypeScript logo" />
+                </button>
 
             <h3> Welcome to ErdPay</h3>
             <p> Select an action to perform</p>
-        </header>
-        <div class="button_container">
-            <button class = "button sqrButton" id="transfer">Transfer</button>
-            <button class = "button sqrButton" id="balance">View Balance</button>
+            </header>
+            <div class="button_container">
+                <button class = "button sqrButton" id="transfer">Transfer</button>
+                <button class = "button sqrButton" id="balance">View Balance</button>
+            </div>
         </div>
     `;
-    const transfer = document.querySelector<HTMLButtonElement>('#transfer');
-    transfer?.addEventListener('click', () => {
-        htmlCreateSessionForTransfer(html)
 
-    });
+  const transfer = document.querySelector<HTMLButtonElement>("#transfer");
+  transfer?.addEventListener("click", () => {
+    htmlCreateSessionForTransfer(html);
+  });
 
-    const balance = document.querySelector<HTMLButtonElement>('#balance')
-    balance?.addEventListener('click', () => {
-        htmlBalance(html)
-    })
+  const balance = document.querySelector<HTMLButtonElement>("#balance");
+  balance?.addEventListener("click", () => {
+    htmlBalance(html);
+  });
 }
 
 export function makeWidgetButton() {
-    const html = document.querySelector<HTMLDivElement>('#app')!
-    html.innerHTML = `
+  const html = document.querySelector<HTMLDivElement>("#app")!;
+  html.innerHTML = `
     <div>
         <button id="mainButton" type="button">
             <img src="https://nifty.erdstall.dev/static/media/erdstall-logo.4ca5436f.png" class="logo" alt="TypeScript logo" />
         </button>
     </div>
     `;
-    const btn = document.querySelector('#mainButton');
-    btn?.addEventListener('click', () => {
-        widget(html)
-    })
+  const btn = document.querySelector("#mainButton");
+  btn?.addEventListener("click", () => {
+    widget(html);
+  });
 }
 
-makeWidgetButton()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+makeWidgetButton();
